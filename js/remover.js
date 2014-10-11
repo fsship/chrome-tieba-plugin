37521b636a0b11728063ae7b7b3e5278b9c284e4
fsship <1844695939@qq.com>
2014-10-11 14:27:28 +0800

    ad remover&spam fliter done

--- js/remover.js
+++ js/remover.js
@@ -1,4 +1,13 @@
-(function() {
+setTimeout(function() {
+    function removeFloor(spam) {
+        $(".l_post").each(function() {
+            if ($(this).text().indexOf(spam) >= 0) {
+                console.log("spam found");
+                $(this).remove();
+            };
+        });
+    };
+
     console.log("Starting Jobs");
     chrome.storage.sync.get({
         settings : {
@@ -6,13 +15,23 @@
             'spamList' : []
         }
     }, function(item) {
+        console.log('settings loaded');
         var settings = item.settings;
+        console.log(settings);
         if (settings.removeAd) {
-            // TODO
-            console.log("removeAd");
+            $("#j_ten_years").remove();
+            $(".BAIDU_CLB_AD").remove();
+            $(".spreadad").remove();
+            $(".banner_post").remove();
+            $(".dasense").remove();
+            $(".per_list").remove();
+            $(".dasense").remove();
+            $(".u9_head").remove();
         };
+        console.log("spam removing start");
         for (var i = 0; i < settings.spamList.length; i++) {
             console.log(settings.spamList[i]);
+            removeFloor(settings.spamList[i]);
         }
     });
-})()
+}, 1000);
